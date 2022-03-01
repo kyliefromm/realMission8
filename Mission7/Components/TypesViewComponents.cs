f71eed5283e28ffce53ca79353eec18d32d3016a
kyliefromm <90372645+kyliefromm@users.noreply.github.com>
2022-02-28 23:05:31 -0700

    fixing views and models

---
+++ Mission7/Components/TypesViewComponents.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Mission7.Models;
+using WaterProject.Models;
+//own model and own words could be named anyting
+namespace WaterProject.Components
+{
+    public class CategoryViewComponent : ViewComponent
+    {
+        private IBookRepository repo { get; set; }
+
+        public CategoryViewComponent(IBookRepository temp)
+        {
+            repo = temp;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+
+            ViewBag.SelectedCategory = RouteData?.Values["Cateogry"];
+            var types = repo.Books
+                .Select(x => x.Category)
+                .Distinct()
+                .OrderBy(x => x);
+            return View(types);
+        }
+    }
+
+
+}
